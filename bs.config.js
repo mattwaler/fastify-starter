03ef8c19ac8bb7bf82d1feaecf9ace7d8f86aef3
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2023-04-07 18:37:38 -0400

    Removing browser-sync, adding delay helper, adding loading states

--- bs.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  files: ['./views/**/*', './public/**/*'],
-  ignore: ['node_modules', '.git'],
-  proxy: 'http://localhost:3000',
-  notify: false,
-  reloadDebounce: 500,
-  reloadDelay: 500,
-  open: false,
-}
---
+++ helpers/delay.js
@@ -0,0 +1,5 @@
+module.exports = async function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), ms)
+  })
+}
